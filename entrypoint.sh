c312433bab58f42d0c45754a799c000abaa450ba
damir.kasen92 <damir.kasen92@gmail.com>
2025-12-11 21:50:35 +0500

    fix bug when stimulus does not work properly

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,10 +2,12 @@
 set -e  # остановить скрипт при любой ошибке
 
 echo "Очистка кэша..."
-php bin/console cache:clear
+php bin/console cache:clear --no-warmup
+php bin/console cache:warmup
 
 echo "Установка ассетов..."
 php bin/console importmap:install --no-interaction
+php bin/console importmap:dump --no-interaction
 php bin/console assets:install --no-interaction
 php bin/console asset-map:compile
 
