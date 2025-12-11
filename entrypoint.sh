cbc7bf9d3b9fedc0174d2c45fc6861b676adfda1
damir.kasen92 <damir.kasen92@gmail.com>
2025-12-11 21:57:29 +0500

    forgot to remove /assets from gitignore

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,12 +2,10 @@
 set -e  # остановить скрипт при любой ошибке
 
 echo "Очистка кэша..."
-php bin/console cache:clear --no-warmup
-php bin/console cache:warmup
+php bin/console cache:clear
 
 echo "Установка ассетов..."
 php bin/console importmap:install --no-interaction
-php bin/console importmap:dump --no-interaction
 php bin/console assets:install --no-interaction
 php bin/console asset-map:compile
 
